ff24906150c93045804b0a08bdd4807fdf14b255
Dominique VAUTRELLE <Dom@imac-27-de-dom.local>
2017-01-01 16:14:40 +0100

    Initial Commit

---
+++ CheckerITunes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CheckerITunes
+//
+//  Created by Dominique VAUTRELLE on 01/01/2017.
+//  Copyright © 2017 Dominique VAUTRELLE. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CheckerITunes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CheckerITunes
+//
+//  Created by Dominique VAUTRELLE on 01/01/2017.
+//  Copyright © 2017 Dominique VAUTRELLE. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
